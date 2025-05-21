ecad4fa327d461282c5a347d92d1a940f93e8feb
Victoroide <cvictorhugo39@gmail.com>
2025-05-21 17:06:45 -0400

    refactor: Remove schema and documentation paths from URL patterns

--- base/urls.py
+++ base/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     
     path('api/auth/', include('app.authentication.urls')),
     path('api/academic/', include('app.academic.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns = get_spectacular_urls() + urlpatterns
